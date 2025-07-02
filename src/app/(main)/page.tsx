e9eadac27202c837bf3d42d76f6a2101d67db8fd
Larina Fu <lrf59@cornell.edu>
2025-07-01 22:27:34 -0400

    [7/1/25] Edits to landing page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const bannerStyle = "text-5xl md:text-6xl lg:text-8xl";
-
 const SAMPLE_PATTERNS = [
   {
     src: "/photos/patterns/repeating-hearts.png",
@@ -18,8 +16,10 @@ const SAMPLE_PATTERNS = [
 export default function Home() {
   return (
     <>
-      <div className={`${bannerStyle}`}>
-        <h1 className="m-4 mt-0 pt-4">Create knitting charts for free.</h1>
+      <div className={`p-2`}>
+        <h1 className="text-center pt-4 text-5xl md:text-6xl lg:text-8xl">
+          Create knitting charts for free.
+        </h1>
         <div className="relative">
           <Image
             src="/yarn-thread.svg"
@@ -51,9 +51,11 @@ export default function Home() {
         </div>
       </div>
       <div
-        className={`text-8xl p-2 flex justify-evenly items-center bg-amaranth-light`}
+        className={`p-2 flex flex-col lg:flex-row justify-evenly items-center bg-amaranth-light`}
       >
-        <h1 className="m-4 w-1/4">Effortlessly edit across all devices</h1>
+        <h1 className="m-4 lg:w-1/3 text-5xl md:text-6xl lg:text-8xl">
+          Effortlessly edit across all devices
+        </h1>
         <div className="flex justify-center">
           <div className="relative m-4">
             <Image
@@ -62,7 +64,7 @@ export default function Home() {
               width={1000}
               height={1000}
               objectFit="contain"
-              className="absolute bottom-0 left-0 border-4 lg:border-8 border-gray-900 rounded-3xl size-3/4 w-auto z-30"
+              className="absolute bottom-0 left-0 border-4 md:border-6 lg:border-8 border-gray-900 rounded-xl lg:rounded-3xl size-3/4 w-auto z-30"
             />
             <div className="pl-5 pb-4">
               <Image
@@ -70,43 +72,52 @@ export default function Home() {
                 alt={""}
                 width={1000}
                 height={1000}
-                className="border-4 lg:border-8 border-gray-700 w-full rounded-3xl z-20"
+                className="border-4 md:border-6 lg:border-8 border-gray-700 w-full rounded-xl lg:rounded-3xl z-20"
               />
             </div>
           </div>
         </div>
       </div>
-      <div className={`${bannerStyle} p-2 flex`}>
+      <div
+        className={`p-2 pt-10 flex flex-col-reverse lg:flex-row items-center`}
+      >
         <div className="p-4 flex flex-col lg:flex-row justify-center items-center">
           <Image
             src={"/photos/earth.jpg"}
-            width={400}
-            height={400}
+            width={1000}
+            height={1000}
             alt="earth"
-            className="border border-black size-3/4 lg:size-1/4"
+            className="border border-black sm:w-1/2 lg:w-1/3"
           />
           <Image
             src={"/curve-right-arrow.svg"}
-            width={100}
-            height={100}
+            width={400}
+            height={400}
             alt="arrow"
-            className="rotate-90 lg:rotate-0 size-1/3 lg:size-1/6"
+            className="hidden lg:block lg:rotate-0 size-1/4"
+          />
+          <Image
+            src={"/curve-down-arrow.svg"}
+            width={400}
+            height={400}
+            alt="arrow"
+            className="lg:hidden size-1/6"
           />
           <Image
             src="/photos/earth-pixelated.png"
-            width={400}
-            height={400}
+            width={1000}
+            height={1000}
             alt="pixelated earth"
-            className="size-3/4 lg:size-1/4"
+            className="sm:w-1/2 lg:w-1/3"
           />
         </div>
-        <h1 className="m-4">
+        <h1 className="m-4 lg:w-1/2 text-5xl md:text-6xl lg:text-8xl">
           Instantly generate color charts from uploaded images.
         </h1>
       </div>
       <Link
         href="create"
-        className="block sticky bottom-2 right-0 ml-auto button text-3xl rounded-3xl w-fit m-5 z-30"
+        className="block sticky bottom-2 right-0 ml-auto button text-4xl md:text-5xl rounded-3xl w-fit m-5 z-30"
       >
         create now
       </Link>
