d0eda2f2fa440e54caec394f59c7ccfb8594361d
Larina Fu <lrf59@cornell.edu>
2025-07-07 22:48:56 -0500

    [7/7/25] Reformatted Create Form

--- src/app/(main)/create/page.tsx
+++ src/app/(main)/create/page.tsx
@@ -1,91 +1,11 @@
-"use client";
+import { Metadata } from "next";
+import Create from "./create";
 
-import { useRef, useState } from "react";
-import Image from "next/image";
+export const metadata: Metadata = {
+  title: "Create Knitting Chart",
+  description: "Create free knitting chart.",
+};
 
-import styles from "./page.module.css";
-import CreateWithImage from "@/components/pixelGrid/createWithImage/createWithImage";
-import useScrollIntoView from "@/hooks/general/useScrollIntoView";
-import InitCustomization from "@/components/pixelGrid/initCustomization/initCustomization";
-
-type BaseOption = "image" | "scratch";
-
-const cardHeaderStyle = "text-xl sm:text-4xl mt-2";
-const cardSubheaderStyle = "text-m sm:text-xl md:text-3xl m-4";
-
-export default function Create() {
-  const [baseOption, setBaseOption] = useState<BaseOption | null>(null);
-  const baseOptionRef = useRef(null);
-  const handleBaseChoice = (option: BaseOption) => {
-    setBaseOption(option);
-  };
-
-  useScrollIntoView(baseOptionRef, [baseOption]);
-
-  const addSelectedBorder = (card: BaseOption) => {
-    if (card === baseOption) {
-      return styles.selected;
-    }
-    return "";
-  };
-
-  return (
-    <div className={`fadeIn`}>
-      <h1 className="mt-2 mb-2 text-center text-4xl md:text-5xl">
-        Create New Pattern
-      </h1>
-      <p className="text-center mb-6 text-2xl md:text-3xl">
-        How would you like to start your pattern?
-      </p>
-      <section className="flex justify-evenly">
-        <div
-          className={`card m-2 hover:cursor-pointer ${
-            styles.card
-          } ${addSelectedBorder("image")}`}
-          onClick={() => handleBaseChoice("image")}
-          tabIndex={0}
-        >
-          <h3 className={cardHeaderStyle}>Upload image</h3>
-          <p className={cardSubheaderStyle}>
-            We&apos;ll match the initial colors on your grid to fit your image
-          </p>
-          <Image
-            src="/image.svg"
-            alt="image"
-            width={100}
-            height={100}
-            className="m-2 size-1/3"
-          />
-        </div>
-        <div
-          className={`card m-2 hover:cursor-pointer ${
-            styles.card
-          } ${addSelectedBorder("scratch")}`}
-          onClick={() => handleBaseChoice("scratch")}
-          tabIndex={0}
-        >
-          <h3 className={cardHeaderStyle}>Start from scratch</h3>
-          <p className={cardSubheaderStyle}>
-            We&apos;ll start you off with a blank grid
-          </p>
-          <Image
-            src="/grid.svg"
-            alt="grid denoting blank pattern"
-            width={100}
-            height={100}
-            className="m-2 size-1/3"
-          />
-        </div>
-      </section>
-      {baseOption && (
-        <section ref={baseOptionRef}>
-          {baseOption === "image" ? (
-            <CreateWithImage />
-          ) : (
-            baseOption === "scratch" && <InitCustomization />
-          )}
-        </section>
-      )}
-    </div>
-  );
+export default function CreatePage() {
+  return <Create />;
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main className="min-h-dvh">{children}</main>
       <Footer />
     </>
   );
